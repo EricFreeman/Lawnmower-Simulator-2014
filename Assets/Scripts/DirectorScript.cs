80a194eb7d628657cf1b4696a727edaa8b816c5b
Eric Freeman <eric.freeman@extendhealth.com>
2014-03-24 17:46:28 -0600

    initial commit just to get it from laptop to desktop

---
+++ Assets/Scripts/DirectorScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Models;
+using UnityEngine;
+
+public class DirectorScript : MonoBehaviour
+{
+    private Map _map;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _map = CreateMap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private Map CreateMap()
+    {
+        var m = new Map();
+
+        var width = 10;
+        var height = 10;
+
+        m.Tiles = new Tile[width,height];
+
+        for (int x = 0; x < width; x++)
+            for (int y = 0; y < height; y++)
+            {
+                var t = new Tile {GameObject = GameObject.CreatePrimitive(PrimitiveType.Cube)};
+                t.GameObject.transform.position = new Vector3(x, 0, y);
+                t.GameObject.renderer.material = new Material("Diffuse");
+                m.Tiles[x, y] = t;
+            }
+
+        return m;
+    }
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float PlayerMoveSpeed = .1f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Input.GetAxisRaw("Horizontal") * PlayerMoveSpeed, 
+                            0, 
+                            Input.GetAxisRaw("Vertical") * PlayerMoveSpeed);	
+	}
+}
