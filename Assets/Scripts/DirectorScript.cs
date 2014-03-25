468c3afc921f15476b7bba5ee57edcc2954aeb6b
Eric Freeman <eric.freeman@extendhealth.com>
2014-03-24 21:08:50 -0600

    quick and dirty loading map from xml file

--- Assets/Scripts/DirectorScript.cs
+++ Assets/Scripts/DirectorScript.cs
@@ -1,41 +1,56 @@
-﻿using Assets.Scripts.Models;
+﻿using System.Xml;
+using Assets.Scripts.Models;
 using UnityEngine;
 
 public class DirectorScript : MonoBehaviour
 {
     private Map _map;
+
     public Transform TileModel;
+    public float TileSize = 2;
 
-	// Use this for initialization
-	void Start ()
-	{
-	    _map = CreateMap();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    
-	}
-
-    private Map CreateMap()
+    // Use this for initialization
+    void Start()
     {
-        var m = new Map();
+        _map = new Map();
+        CreateMap();
+    }
 
-        var width = 10;
-        var height = 10;
+    // Update is called once per frame
+    void Update()
+    {
 
-        m.Tiles = new Tile[width,height];
+    }
 
-        for (int x = 0; x < width; x++)
-            for (int y = 0; y < height; y++)
+    private void CreateMap()
+    {
+        CreateTiles("TestLevel");
+    }
+
+    private void CreateTiles(string name)
+    {
+        var doc = new XmlDocument();
+        doc.Load("Assets/Resources/Levels/" + name + ".xml");
+
+        var level = doc.SelectSingleNode("/Level");
+        var rows = level.SelectNodes("Row");
+        var c = rows[0].SelectNodes("Column");
+
+        _map.Tiles = new Tile[c.Count, rows.Count];
+
+        for (int y = 0; y < rows.Count; y++)
+        {
+            var columns = rows[y].SelectNodes("Column");
+            for (int x = 0; x < columns.Count; x++)
             {
+                var currentNode = rows[y].SelectNodes("Column")[x];
                 var t = new Tile { Transform = Instantiate(TileModel) as Transform };
-                t.Transform.position = new Vector3(x, 0, y);
-                t.Transform.renderer.material.mainTexture = m.Materials[1];
+                t.Transform.position = new Vector3(x * TileSize, 0, y * TileSize);
+                t.Transform.localScale *= TileSize;
+                t.Transform.renderer.material.mainTexture = _map.Materials[int.Parse(currentNode.SelectSingleNode("Tile").InnerText)];
                 t.Transform.gameObject.SetActive(true);
-                m.Tiles[x, y] = t;
+                _map.Tiles[x, y] = t;
             }
-
-        return m;
+        }
     }
 }
