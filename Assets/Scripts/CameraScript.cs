e9d12dd26189b4096e58facb92d99a6b0bee61b4
Eric Freeman <eric.freeman@extendhealth.com>
2014-03-26 19:43:02 -0600

    updated camera to follow player with independent player rotation

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public Transform Target;
+    public float Distance = 5f;
+
+	void Update ()
+	{
+	    transform.position = Target.position + (Vector3.up*Distance);
+	}
+}
---
+++ Assets/Scripts/LookAtMouse.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour
+{
+    public float speed;
+
+    void FixedUpdate()
+    {
+        var playerPlane = new Plane(Vector3.up, transform.position);
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        var hitdist = 0.0f;
+        if (playerPlane.Raycast(ray, out hitdist))
+        {
+            var targetPoint = ray.GetPoint(hitdist);
+            var targetRotation = Quaternion.LookRotation(targetPoint - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, speed * Time.time);
+        }
+    }
+}
