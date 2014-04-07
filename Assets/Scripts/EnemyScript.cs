619db5c16d0e4d48522ce7302b138478fc28d2af
Eric Freeman <eric.freeman@extendhealth.com>
2014-04-07 08:13:52 -0600

    enemies will chase after player, or if out of sight, last known position of player

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -3,9 +3,11 @@
 public class EnemyScript : MonoBehaviour
 {
     public GameObject Player;
-    public float FieldOfView;
+    public float FieldOfView = 60f;
+    public float ViewDistance = 5.5f;
+    public float MoveSpeed = 2f;
 
-    private bool isAlerted;
+    private bool canSeePlayer;
 
     public EnemyState State = EnemyState.Patrolling;
     private Vector3 LastKnownLocation;
@@ -15,23 +17,44 @@ public class EnemyScript : MonoBehaviour
 
     }
 
-    void Update()
+    void FixedUpdate()
     {
+        canSeePlayer = false;
         Vector3 rayDirection = Player.transform.position - transform.position;
- 
+
        // Detect if player is within the field of view
         if ((Vector3.Angle(rayDirection, transform.forward)) < FieldOfView)
         {
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, rayDirection, out hit))
-            {
+            if (Physics.Raycast(transform.position, rayDirection, out hit) && Vector3.Distance(transform.position, Player.transform.position) < ViewDistance)
                 if (hit.transform.tag == "Player")
-                {
-                    renderer.material.color = Color.yellow;
-                    Debug.Log("Can see player");
-                    isAlerted = true;
-                }
-            }
+                    canSeePlayer = true;
+        }
+
+        switch (State)
+        {
+            case EnemyState.Idle:
+                if(canSeePlayer) State = EnemyState.Alert;
+                break;
+            case EnemyState.Patrolling:
+                if (canSeePlayer) State = EnemyState.Alert;
+                break;
+            case EnemyState.Alert:
+                AlertState();
+                break;
+        }
+    }
+
+    private void AlertState()
+    {
+        if (!canSeePlayer)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, LastKnownLocation, MoveSpeed * Time.deltaTime);
+        }
+        else
+        {            
+            LastKnownLocation = Player.transform.position;
+            transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, MoveSpeed * Time.deltaTime);
         }
     }
 }
